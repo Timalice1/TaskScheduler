743a020816f6c88e1b817f7af5ba26f66b382012
Timalice1 <newacccc2@gmail.com>
2022-06-23 09:39:20 +0300

    v1.0

---
+++ TaskScheduler/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Text;
+using System.Windows;
+
+namespace TaskScheduler {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Get all task from Windows using Microsoft.Win32.TaskSheduler.TaskService
+        /// & load it to TaskList
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            try {
+                var folder = TaskService.Instance.RootFolder;
+                foreach (var task in folder.Tasks) {
+                    MyTask t = new MyTask() {
+                        Name = task.Name,
+                        LastRunTime = task.LastRunTime,
+                        NextRunTime = task.NextRunTime,
+                        File = task.Definition.Actions[0].ToString(),
+                    };
+                    if (task.Definition.Triggers.Count > 1) {
+                        StringBuilder sb = new StringBuilder();
+                        foreach(var trigger in task.Definition.Triggers) 
+                            sb.Append(trigger.ToString() + "; ");
+                        t.Trigger = sb.ToString();
+
+                    } else if (task.Definition.Triggers.Count == 1)
+                        t.Trigger = task.Definition.Triggers[0].ToString();
+                    TaskList.Items.Add(t);
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Create new object for MyTask using NewTask form
+        /// Add it to TaskList & register by using StartTask method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddTask_Click(object sender, RoutedEventArgs e) {
+            NewTask newTask = new NewTask();
+            var res = newTask.ShowDialog();
+
+            if (res == true) {
+                //Create new task object
+                MyTask task = new MyTask() {
+                    Name = newTask.Name,
+                    File = newTask.RunFile,
+                    StartDateTime = newTask.StartDateTime,
+                    Trigger = newTask.Trigger,
+                    Interval = newTask.Interval,
+                    Arguments = newTask.Arguments
+                };
+
+                //Run Task
+                var t = StartTask(task);
+                task.LastRunTime = t.LastRunTime;
+                task.NextRunTime = t.NextRunTime;
+
+                //Add task to TaskList
+                TaskList.Items.Add(task);
+            }
+
+        }
+
+        /// <summary>
+        /// Create & execute the task using Microsoft.Win32.TaskSheduler namespace
+        /// </summary>
+        /// <param name="task">Task definition</param>
+        private Task StartTask(MyTask task) {
+            // Get the service on the local machine
+            using (TaskService ts = new TaskService()) {
+                // Create a new task definition and assign properties
+                TaskDefinition td = ts.NewTask();
+                td.Actions.PowerShellConversion = PowerShellActionPlatformOption.All;
+                td.RegistrationInfo.Description = "Does something";
+
+                // Create a defined triggers for task
+                if (task.Trigger == "Once")
+                    td.Triggers.Add(new TimeTrigger() { StartBoundary = task.StartDateTime });
+                else {
+                    td.Triggers.Add(new DailyTrigger() { DaysInterval = task.Interval, StartBoundary = task.StartDateTime });
+                }
+                // Create an action for task
+                td.Actions.Add(task.File);
+
+                //Run process even if laptop not charging
+                td.Settings.DisallowStartIfOnBatteries = false;
+
+                // Register the task in the root folder
+                ts.RootFolder.RegisterTaskDefinition($@"{task.Name}", td);
+
+                return ts.GetTask(task.Name);
+            }
+
+        }
+
+        private void TaskList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
+            if(TaskList.SelectedItem == null) {
+                btnDeleteTask.IsEnabled = false;
+                btnRunNow.IsEnabled = false;
+            }
+            btnDeleteTask.IsEnabled = true;
+            btnRunNow.IsEnabled = true;
+        }
+
+        //Execute task immediately
+        private void btnRunNow_Click(object sender, RoutedEventArgs e) {
+            var task = TaskList.SelectedItem as MyTask;
+            TaskService.Instance.GetTask(task.Name).Run();
+        }
+
+        //Delete task from list & root folder
+        private void btnDeleteTask_Click(object sender, RoutedEventArgs e) {
+            try {
+                //Get task from instance
+                var task = TaskList.SelectedItem as MyTask;
+                TaskService ts = new TaskService();
+
+                // Check to make sure account privileges allow task deletion
+                var identity = WindowsIdentity.GetCurrent();
+                var principal = new WindowsPrincipal(identity);
+                if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+                    throw new Exception($"Cannot delete task with your current identity '{identity.Name}' permissions level." +
+                    "You likely need to run this application 'as administrator' even if you are using an administrator account.");
+
+                //Delete task
+                ts.RootFolder.DeleteTask(task.Name);
+                TaskList.Items.RemoveAt(TaskList.SelectedIndex);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Describe the Task object
+    /// </summary>
+    class MyTask {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string File { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime LastRunTime { get; set; }
+        public DateTime NextRunTime { get; set; }
+        public string Trigger { get; set; }
+        public short Interval { get; set; }
+        public string Arguments { get; set; }
+    }
+}
---
+++ TaskScheduler/NewTask.xaml.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace TaskScheduler {
+    /// <summary>
+    /// Логика взаимодействия для NewTask.xaml
+    /// </summary>
+    public partial class NewTask : Window {
+
+        #region Properties
+        public new string Name { get; private set; }
+        public string Description { get; private set; }
+        public string RunFile { get; private set; }
+        public DateTime StartDateTime { get; private set; }
+        public string Trigger { get; private set; }
+        public short Interval { get; private set; }
+        public string Arguments { get; private set; }
+        #endregion
+
+        public NewTask() {
+            InitializeComponent();
+            DatePickerOnce.DisplayDateStart = DateTime.Now;
+            DatePickerDaily.DisplayDateStart = DateTime.Now;
+            DatePickerOnce.SelectedDate = DateTime.Now;
+            DatePickerDaily.SelectedDate = DateTime.Now;
+            for (int i = 0; i < 24; i++) {
+                if (i < 10) {
+
+                    cbHoursTriggerOnce.Items.Add("0" + i);
+                    cbHoursTriggerDaily.Items.Add("0" + i);
+                }
+                else {
+
+                    cbHoursTriggerOnce.Items.Add(i);
+                    cbHoursTriggerDaily.Items.Add(i);
+                }
+            }
+            for (int i = 0; i < 60; i++) {
+                if (i < 10) {
+                    cbMinutesTriggerOnce.Items.Add("0" + i);
+                    cbMinutesTriggerDaily.Items.Add("0" + i);
+                }
+                else {
+                    cbMinutesTriggerOnce.Items.Add(i);
+                    cbMinutesTriggerDaily.Items.Add(i);
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e) {
+            try {
+                CheckData();
+                Name = tbName.Text;
+                RunFile = tbFile.Text;
+                Arguments = tbArguments.Text;
+
+                //Triggers for once
+                if (rbOnce.IsChecked == true) {
+                    string startDate = DatePickerOnce.Text;
+                    string startTime = $"{cbHoursTriggerOnce.Text}:{cbMinutesTriggerOnce.Text}";
+                    StartDateTime = DateTime.Parse(startDate + " " + startTime);
+                    Trigger = "Once";
+                }
+
+                //Triggers for daily
+                if (rbDaily.IsChecked == true) {
+                    string startDate = DatePickerDaily.Text;
+                    string startTime = $"{cbHoursTriggerDaily.Text}:{cbMinutesTriggerDaily.Text}";
+                    StartDateTime = DateTime.Parse(startDate + " " + startTime);
+                    Trigger = $"Every {tbRepeatDaily.Text} days";
+                    Interval = short.Parse(tbRepeatDaily.Text);
+                }
+
+                DialogResult = true;
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CheckData() {
+            //Check for empty fields
+            if (string.IsNullOrEmpty(tbName.Text) |
+                string.IsNullOrEmpty(tbFile.Text))
+                throw new Exception("Please, fill all required fields");
+
+            //Check for existing file
+            if (!File.Exists(tbFile.Text))
+                throw new Exception("File not exist...");
+
+            //Check for selecting event time
+            if (rbDaily.IsChecked != true & rbOnce.IsChecked != true)
+                throw new Exception("Please, select the event time");
+
+            //Check data in selected tab
+            if (rbOnce.IsChecked == true) {
+                if (DatePickerOnce.Text == "" | cbHoursTriggerOnce.Text == "" | cbMinutesTriggerOnce.Text == "")
+                    throw new Exception("Please, select time & date");
+                return;
+            }
+
+            if (rbDaily.IsChecked == true) {
+                if (DatePickerDaily.Text == "" | cbHoursTriggerDaily.Text == "" | cbMinutesTriggerDaily.Text == "")
+                    throw new Exception("Please, select time & date");
+                if (string.IsNullOrEmpty(tbRepeatDaily.Text))
+                    throw new Exception("Please, select repeat time");
+
+                return;
+            }
+
+        }
+
+        #region
+        private void rbOnce_Checked(object sender, RoutedEventArgs e) {
+            tabControl.SelectedIndex = 0;
+        }
+
+        private void rbDaily_Checked(object sender, RoutedEventArgs e) {
+            tabControl.SelectedIndex = 1;
+        }
+
+        private void rbWeekly_Checked(object sender, RoutedEventArgs e) {
+            tabControl.SelectedIndex = 2;
+        }
+        #endregion
+
+        private void tbRepeatDaily_TextChanged(object sender, TextChangedEventArgs e) {
+            var regex = new Regex(@"\d+");
+            if (!regex.IsMatch(tbRepeatDaily.Text)) {
+                tbRepeatDaily.Text = "";
+                return;
+            }
+            if (int.Parse(tbRepeatDaily.Text) <= 0) {
+                tbRepeatDaily.Text = "";
+                return;
+            }
+        }
+
+        private void btnExplore_Click(object sender, RoutedEventArgs e) {
+            try {
+                OpenFileDialog ofd = new OpenFileDialog();
+                var res = ofd.ShowDialog();
+
+                if (res == true) {
+                    tbFile.Text = ofd.FileName;
+                }
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
